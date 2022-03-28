0a9d208fbca89ad625d6d136092f9e2511787691
Антон Януков <goliaf551@mail.ru>
2022-03-28 17:59:53 +0600

    First commit

---
+++ app/src/main/java/com/example/lesson3/MainActivity.kt
@@ -0,0 +1,25 @@
+package com.example.lesson3
+
+/*Bottom menu через Navigation component*/
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import androidx.navigation.ui.setupWithNavController
+import com.example.lesson3.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
+    lateinit var navController: NavController
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        navController = Navigation.findNavController(this, R.id.nav_host_fragment)
+
+        binding.bottomNavView.setupWithNavController(navController)
+    }
+}
---
+++ app/src/main/java/com/example/lesson3/screens/first/FirstFragment.kt
@@ -0,0 +1,18 @@
+package com.example.lesson3.screens.first
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.lesson3.R
+
+class FirstFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_first, container, false)
+    }
+}
